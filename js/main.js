36bc2c7e5b63ae7dd8ab49d5be530a1ff7083241
pratikmaity00 <pratikmaity27910@gmail.com>
2026-01-18 10:21:59 +0530

    changes in path on navbar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function togglerOn() {
+    document.querySelector('.navlinks').style.display = 'block';
+    
+}
+
+function togglerOff() {
+    document.querySelector('.navinks').navlinks.style.display = 'none';
+    
+}
